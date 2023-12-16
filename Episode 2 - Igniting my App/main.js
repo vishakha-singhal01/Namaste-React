a65f2ed5abe2c348a3faa889a06f76ff607c15ab
Vishakha Singhal01 <singhalvishakha.career@gmail.com>
2023-12-16 21:46:17 +0530

    Second commit

---
+++ Episode 2 - Igniting my App/main.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// Manipulate the HTML DOM using React
+
+// Create nested React Elements
+const heading = React.createElement(
+    "h1",
+    {
+      id: "title",
+      style: {
+        background:"red",
+      },
+      className:"title"
+    },
+    "heading by parcel"
+  );
+  const heading1 = React.createElement(
+    "h1",
+    {
+      id: "title",
+    },
+    "heading1"
+  );
+  
+  const container = React.createElement(
+    "div",
+    {
+      id: "container",
+    },
+    [heading, heading1]
+  );
+  
+  // create root using createRoot
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  // passing react element inside root
+  root.render(container);
